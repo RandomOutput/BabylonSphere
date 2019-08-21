92d581ce512a526f30dac96f0ec0633a6fd64e5d
Daniel Plemmons <dpx109@gmail.com>
2019-08-20 21:12:48 -0700

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
+import * as React from 'react';
+import * as BABYLON from 'babylonjs';
+import BabylonScene, { SceneEventArgs } from './SceneComponent'; // import the component above linking to file we just created.
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class PageWithScene extends React.Component<{}, {}> {
+      onSceneMount(e: SceneEventArgs) : void {
+        const { canvas, scene, engine } = e;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        scene.clearColor = new BABYLON.Color4(1.0, 1.0, 1.0, 1.0);
+
+        // This creates and positions a free camera (non-mesh)
+        var camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2.0, Math.PI / 2.0, 40, new BABYLON.Vector3(0,0,0), scene);
+
+        // This targets the camera to scene origin
+        camera.setTarget(BABYLON.Vector3.Zero());
+
+        // This attaches the camera to the canvas
+        camera.attachControl(canvas, true);
+
+        // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
+        var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 20, scene);
+        sphere.rotate(new BABYLON.Vector3(0,0,1), Math.PI, BABYLON.Space.LOCAL);
+        sphere.flipFaces(true);
+
+        var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
+        myMaterial.disableLighting = true;
+        myMaterial.emissiveTexture = new BABYLON.Texture("Copenhagen.jpg", scene);
+
+        sphere.material = myMaterial;
+
+        // Move the sphere upward 1/2 its height
+        sphere.position.y = 0;
+
+        engine.runRenderLoop(() => {
+            if (scene) {
+                scene.render();
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <BabylonScene height={400} width={800} onSceneMount={this.onSceneMount} />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <PageWithScene />,
+    document.getElementById('root')
+  );
