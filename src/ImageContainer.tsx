fe6fb38bc58a4de99f51b56fd4d41c3d442c5725
Daniel Plemmons <dpx109@gmail.com>
2019-08-31 09:31:19 -0700

    add some button styling

--- src/ImageContainer.tsx
+++ src/ImageContainer.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faFileImage } from '@fortawesome/free-solid-svg-icons'
 
 export default (props: any) => {
   return (
     <div className="image-container">
       <span className='image-name'></span>
-      <label className="file-label" htmlFor="file-selector-01"> 
+      <label className="file-label" htmlFor="file-selector-01">
+        <FontAwesomeIcon icon={faFileImage} />
         Select File
       </label>
       <input type='file' id="file-selector-01" className="file-selector" onChange={props.onChange} />
